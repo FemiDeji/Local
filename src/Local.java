48eff11a4ee0ea8f5cd2584b24dd9fc28c3ddfef
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 14:47:47 -0800

    Initial commit

---
+++ src/Local.java
@@ -0,0 +1,7 @@
+public class Local{
+    public static void main(String[] args){
+        int var = 10; //Declared a Local Variable
+        // This variable is local to this main method only
+        System.out.println("Local Variable:" + var);
+    }
+}
